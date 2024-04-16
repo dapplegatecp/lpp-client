ad791f8cf0faa8508b18a8765dd5ca479e86dd87
Doug Applegate <dapplegate@cradlepoint.com>
2024-04-16 14:35:53 -0600

    included too much

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-if __name__ == "__main__":
-    print("STARTING UP, WAITING FOR INPUT")
-    while True:
-        user_input = input("Enter 'q' to quit: ")
-        if user_input.lower() == 'q':
-            break
-        print("YOU INPUT:", user_input)
